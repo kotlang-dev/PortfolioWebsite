5c96c3ebbe375bacfce78f2475913e3f4ea99f33
Mohammad Arif <codeinkotlang@gmail.com>
2025-10-09 09:17:23 +0530

    theme param removed in MarkdownLayout.kt

--- site/src/jsMain/kotlin/org/kotlang/portfolio/components/layouts/MarkdownLayout.kt
+++ site/src/jsMain/kotlin/org/kotlang/portfolio/components/layouts/MarkdownLayout.kt
@@ -2,8 +2,6 @@ package org.kotlang.portfolio.components.layouts
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
@@ -14,30 +12,13 @@ import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.core.PageContext
 import com.varabyte.kobweb.core.layout.Layout
 import com.varabyte.kobweb.silk.style.toModifier
-import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import kotlinx.browser.window
-import kotlinx.coroutines.delay
 import org.jetbrains.compose.web.css.px
 import org.kotlang.portfolio.theme.MarkdownStyle
 import org.kotlang.portfolio.util.setPageMetadata
-import org.w3c.dom.url.URLSearchParams
 
 @Layout
 @Composable
 fun MarkdownLayout(ctx: PageContext, content: @Composable () -> Unit) {
-
-    var colorMode by ColorMode.currentState
-
-    LaunchedEffect(ctx.route) {
-        delay(100)
-        val theme = window.location.search.let {
-            val params = URLSearchParams(it)
-            params.get("theme")
-        }
-
-        if (theme == "dark") colorMode = ColorMode.DARK
-        else if (theme == "light") colorMode = ColorMode.LIGHT
-    }
 
     LaunchedEffect(Unit) {
         setPageMetadata(
